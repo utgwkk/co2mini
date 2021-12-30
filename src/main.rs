515ecbcc5de5b7a621ff276e6805e2fbda948d00
UTAGAWA Kiki <utagawakiki@gmail.com>
2021-12-30 12:23:55 +0900

    read data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-fn main() {
-    for device in rusb::devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
+use std::time::Duration;
 
-        println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
-            device.bus_number(),
-            device.address(),
-            device_desc.vendor_id(),
-            device_desc.product_id());
+fn main() {
+    let vid = 0x04d9;
+    let pid = 0xa052;
+    let device_handle = rusb::open_device_with_vid_pid(vid, pid);
+    if device_handle.is_none() {
+        return
     }
+    let mut device_handle = device_handle.unwrap();
+    let result = device_handle.claim_interface(0);
+    if result.is_err() {
+        return
+    }
+
+    let buf = &mut [0; 8];
+    let result = device_handle.read_bulk(rusb::constants::LIBUSB_ENDPOINT_IN | 0x1, buf, Duration::new(5, 0));
+    if result.is_err() {
+        return
+    }
+    println!("{:?}", buf);
+    println!("{} {}", tamb(*buf), cntr(*buf))
+}
+
+// http://co2meters.com/Documentation/Other/AN_RAD_0301_USB_Communications_Revised8.pdf
+
+fn tamb(data: [u8; 8]) -> f32 {
+    let msb: f32 = data[1].into();
+    let lsb: f32 = data[2].into();
+    return msb + lsb * 0.01
+}
+
+fn cntr(data: [u8; 8]) -> u16 {
+    let msb: u16 = data[1].into();
+    let lsb: u16 = data[2].into();
+    return msb * 100 + lsb;
 }
